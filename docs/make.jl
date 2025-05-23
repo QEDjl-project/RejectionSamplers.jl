dcee7ccdf1cce19885274bf9b0a2a58d3a8bba48
Uwe Hernandez Acosta <u.hernandez@hzdr.de>
2025-05-23 22:36:43 +0200

    updated gitlab-ci

--- docs/make.jl
+++ docs/make.jl
@@ -28,4 +28,4 @@ makedocs(;
     pages = ["index.md"; numbered_pages],
 )
 
-deploydocs(; repo = "github.com/QEDjl-project/GPUEventGenerators.jl")
+#deploydocs(; repo = "github.com/QEDjl-project/GPUEventGenerators.jl")
