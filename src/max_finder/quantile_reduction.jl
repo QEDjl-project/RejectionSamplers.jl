7e37c442a59a9aa14b80c57a223418b80662fed6
Uwe Hernandez Acosta <u.hernandez@hzdr.de>
2025-07-08 15:57:43 +0200

    added max-finder tests

--- src/max_finder/quantile_reduction.jl
+++ src/max_finder/quantile_reduction.jl
@@ -4,7 +4,7 @@ struct QuantileReductionMethod{T <: Real} <: AbstractSampleBasedMaxFinder
 end
 
 _nsamples(n::QuantileReductionMethod) = n.nsamples
-
+#=
 function _findmax(
         method::QuantileReductionMethod{T},
         weights::AbstractVector{T},
@@ -34,4 +34,22 @@ function _findmax(
     end
     return
 >>>>>>> f029ef4 (added max-finder)
+end
+=#
+function _findmax(
+    method::QuantileReductionMethod{T},
+    weights::AbstractVector{T},
+) where {T<:Real}
+    sorted_weights = sort(weights)
+    sum_weight = sum(sorted_weights)
+    sum_weight_quantile = method.p * sum_weight
+
+    s = 0.0
+    for weight in reverse(sorted_weights)
+        s += weight
+        if s >= sum_weight_quantile
+            return weight
+        end
+    end
+    return
 end
