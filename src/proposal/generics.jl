3e4c219bdb75ac42d7290af58fb0b69ce2e88ee1
Uwe Hernandez Acosta <u.hernandez@hzdr.de>
2025-07-18 17:53:23 +0200

    started refactoring proposal

--- src/proposal/generics.jl
+++ src/proposal/generics.jl
@@ -1,7 +1,21 @@
+
 # generic fallbacks
 
 # use the default rng for given dest-array and fall back to the interface
-function Random.rand!(proposal::AbstractProposal, dest::AbstractVector)
-    rng = default_rng(typeof(dest))
-    return _rand!(rng, proposal, dest)
+function Random.rand!(proposal::AbstractProposal{T}, dest::AbstractVector{T}) where {T}
+    rng = default_rng(dest)
+    _rand!(rng, proposal, dest)
+end
+
+# transformation based proposal
+
+function _rand!(
+    rng::AbstractRNG,
+    proposal::AbstractTransformationBasedProposal{T},
+    dest::AbstractVector{T},
+) where {T}
+    _rand!(rng, UnitCoordSampler(proposal), dest)
+    transform!(proposal, dest)
+
+    return dest
 end
