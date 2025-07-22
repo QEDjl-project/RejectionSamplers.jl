003ac238662cdd995a064752790644f2e66e3068
Uwe Hernandez Acosta <u.hernandez@hzdr.de>
2025-07-22 18:07:20 +0200

    fix after rebase

--- test/max_finder.jl
+++ test/max_finder.jl
@@ -28,7 +28,7 @@ function testsuite_max_finder_gaussian(backend, vec_type, out_dtype, dim)
             dist,
             proposal,
             max_finder;
-            dtype = SVector{dim, out_dtype},
+            dtype = SVector{dim,out_dtype},
         )
 
         @test isapprox(groundtruth, max_val, atol = 1.0e-4)
@@ -43,7 +43,7 @@ function testsuite_max_finder_gaussian(backend, vec_type, out_dtype, dim)
             dist,
             proposal,
             max_finder;
-            dtype = SVector{dim, out_dtype},
+            dtype = SVector{dim,out_dtype},
         )
 
 
@@ -54,7 +54,7 @@ function testsuite_max_finder_gaussian(backend, vec_type, out_dtype, dim)
             #FIXME: quantile estimate seems broken for F16
             if !(out_dtype == Float16)
                 # groundtruth
-                samples = Vector{SVector{dim,dtype}}(undef, n)
+                samples = Vector{SVector{dim,out_dtype}}(undef, n)
                 GPUEventGenerators._rand!(RNG, proposal, samples)
                 weights = sort(GPUEventGenerators._compute.(dist, samples))
                 residual_weights = @. max(1, weights / max_val)
