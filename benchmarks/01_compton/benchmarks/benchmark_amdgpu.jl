c5127296a49a393e598e635c4b88988f6795f095
Uwe Hernandez Acosta <u.hernandez@hzdr.de>
2025-09-13 22:11:33 +0200

    little refac

--- benchmarks/01_compton/benchmarks/benchmark_amdgpu.jl
+++ benchmarks/01_compton/benchmarks/benchmark_amdgpu.jl
@@ -24,18 +24,6 @@ end
 
 BenchmarkTools.DEFAULT_PARAMETERS.seconds = 120.0
 
-"Create random `mu`, `sig`, and `dom` values for given dimension and dtype."
-function create_parameters(dtype)
-    @info "Create parameters"
-    om = dtype(2.0e-3) # 1keV
-    @info "om = $om"
-    lower = dtype.((om, -1.0, 0.0))
-    upper = dtype.((om, 1.0, 2 * pi))
-    dom = (lower, upper)
-
-    return dom
-end
-
 include("benchmarks.jl")
 include("../params.jl")
 
--- benchmarks/01_compton/params.jl
+++ benchmarks/01_compton/params.jl
@@ -13,3 +13,14 @@ const BATCH_SIZES = Int.(2.0 .^ (5:6))
 
 @info "Problem sizes (number of events): $NS"
 @info "Batch sizes: $BATCH_SIZES"
+
+function create_parameters(dtype)
+    @info "Create parameters"
+    om = dtype(2.0e-3) # 1keV
+    @info "om = $om"
+    lower = dtype.((om, -1.0, 0.0))
+    upper = dtype.((om, 1.0, 2 * pi))
+    dom = (lower, upper)
+
+    return dom
+end
