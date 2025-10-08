ee5fff0d1e729103b5f4fb9a331d7e5c49e43a2e
Uwe Hernandez Acosta <u.hernandez@hzdr.de>
2025-10-08 14:07:56 +0200

    switched QEDbase dev to the latest commit before 0.5; can be removed if QEDcore is working again

--- add_dev.jl
+++ add_dev.jl
@@ -3,7 +3,7 @@ using Pkg
 # dev branches from QuantumElectrodynamics.jl
 
 packages = (
-    PackageSpec(name = "QEDbase", rev = "dev"),
+    PackageSpec(name = "QEDbase", rev = "c3861d67767deb3449581c7e71f15a4b3e5f2724"),
     PackageSpec(name = "QEDcore", rev = "dev"),
     PackageSpec(name = "QEDprocesses", rev = "dev"),
 )
