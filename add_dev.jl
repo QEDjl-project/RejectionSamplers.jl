deacd1abfe130e0f22f4a446f051eff6cfe3ce42
Uwe Hernandez Acosta <u.hernandez@hzdr.de>
2025-10-15 20:28:04 +0300

    fixed dev dependencies

--- add_dev.jl
+++ add_dev.jl
@@ -3,8 +3,8 @@ using Pkg
 # dev branches from QuantumElectrodynamics.jl
 
 packages = (
-    PackageSpec(name = "QEDbase", rev = "c3861d67767deb3449581c7e71f15a4b3e5f2724"),
-    PackageSpec(name = "QEDcore", rev = "dev"),
+    #PackageSpec(name = "QEDbase"),
+    #PackageSpec(name = "QEDcore"),
     PackageSpec(name = "QEDprocesses", rev = "dev"),
 )
 
