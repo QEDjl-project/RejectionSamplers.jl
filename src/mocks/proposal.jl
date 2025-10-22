a2ed51cc37e24ee2dbebbdeab3784d36c93638c6
Uwe Hernandez Acosta <szabo137@users.noreply.github.com>
2025-10-22 11:09:11 +0200

    Update src/mocks/proposal.jl
    Co-authored-by: Anton Reinhard <anton.reinhard@proton.me>

--- src/mocks/proposal.jl
+++ src/mocks/proposal.jl
@@ -62,5 +62,6 @@ function RejectionSamplers._propose!(
     n = length(sample_dest)
     steps = T(2 / (n + 1))
     map!(i -> ntuple(x -> -1 + i * steps, D), sample_dest, 1:n)
-    return weight_dest .= KernelAbstractions.ones(backend, T, n)
+    weight_dest .= KernelAbstractions.ones(backend, T, n)
+    return nothing
 end
